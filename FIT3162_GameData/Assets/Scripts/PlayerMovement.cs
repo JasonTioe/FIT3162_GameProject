7048753b7d33db703d8028eceb95a340942c28ce
Jason <jason.tioe@gmail.com>
2025-03-20 11:02:42 +1100

    Updated player movement
    Up and down arrow key inputs added

--- FIT3162_GameData/Assets/Scripts/PlayerMovement.cs
+++ FIT3162_GameData/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,10 @@ public class PlayerMovement : MonoBehaviour
         this.transform.position += Vector3.left * this.speed * Time.deltaTime;
     } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
         this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+    } else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+        this.transform.position += Vector3.up * this.speed * Time.deltaTime;
+    } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+        this.transform.position += Vector3.down * this.speed * Time.deltaTime;
     }
-  }  
+  }
 }
