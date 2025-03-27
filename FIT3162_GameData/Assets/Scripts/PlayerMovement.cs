3c77a2b49734fc2386861581d2945a1bad33b52b
Jason <jason.tioe@gmail.com>
2025-03-27 11:10:06 +1100

    update 27/3/25
    complete player movement, boundary scripts, player idle animation

--- FIT3162_GameData/Assets/Scripts/PlayerMovement.cs
+++ FIT3162_GameData/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public float speed = 5.0f;
+  public float speed = 1.0f;
 
   private void Update()
   {
+    
     if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
         this.transform.position += Vector3.left * this.speed * Time.deltaTime;
     } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
